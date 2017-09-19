e63e5344342fcf8f2e16c6e14f88183608993791
patternoia <patternoia@me.com>
2017-09-19 09:27:52 +0200

    Add FairDbRelationalParSet

---
+++ dbExamples/FairDbCbmSts/src/StsSensorBatch.cxx
@@ -0,0 +1,138 @@
+#include "StsSensorBatch.h"
+#include "StsSensor.h"
+
+ClassImp(StsSensorBatch);
+
+static FairDbGenericParRegistry<StsSensorBatch> ClassRegistry("StsSensorBatch", FairDbDetector::kSts, DataType::kData);
+
+#include "FairDbGenericParSet.tpl"
+template class FairDbGenericParSet<StsSensorBatch>;
+
+#include "FairDbRelationalParSet.tpl"
+template class FairDbRelationalParSet<StsSensorBatch>;
+
+#include "FairDbReader.tpl"
+template class FairDbReader<StsSensorBatch>;
+
+#include "FairDbWriter.tpl"
+template class FairDbWriter<StsSensorBatch>;
+
+StsSensorBatch::StsSensorBatch(FairDbDetector::Detector_t detid,
+              DataType::DataType_t dataid,
+              const char* name,
+              const char* title,
+              const char* context,
+              Bool_t ownership):
+  FairDbRelationalParSet(detid, dataid, name, title, context, ownership),
+  fSensors(NULL),
+  fNumber(""),
+  fDate(ValTimeStamp::GetBOT()),
+  fComment("")
+{
+}
+
+StsSensorBatch::~StsSensorBatch()
+{
+  delete fSensors;
+}
+
+void StsSensorBatch::Print()
+{
+  std::cout << "Sts Sensor Batch Entity Instance:" << std::endl;
+
+  std::cout << "Id = " << fId << std::endl;
+  std::cout << "Number = " << fNumber << std::endl;
+  std::cout << "Date = " << fDate << std::endl;
+  std::cout << "Comment = " << fComment << std::endl;
+
+  std::cout << std::endl;
+}
+
+const TObjArray* StsSensorBatch::GetSensors()
+{
+  if (!fSensors) fSensors = StsSensor::GetByBatchId(fId);
+  return fSensors;
+}
+
+string StsSensorBatch::GetTableDefinition(const char* Name)
+{
+  string sql("create table ");
+  if ( Name ) { sql += Name; }
+  else { sql += GetTableName(); }
+  sql += "( SEQNO             INT NOT NULL,";
+  sql += "  ROW_ID            INT NOT NULL,";
+
+  sql += "  ID INT NOT NULL,";
+  sql += "  NUMBER TEXT,";
+  sql += "  DATE DATETIME,";
+  sql += "  COMMENT TEXT,";
+
+  sql += "  primary key(SEQNO,ROW_ID))";
+  return sql;
+}
+
+void StsSensorBatch::Fill(FairDbResultPool& res_in,
+                        const FairDbValRecord* valrec)
+{
+  res_in >> fId;
+  res_in >> fNumber;
+  res_in >> fDate;
+  res_in >> fComment;
+}
+
+void StsSensorBatch::Store(FairDbOutTableBuffer& res_out,
+                         const FairDbValRecord* valrec) const
+{
+
+  res_out << fId;
+  res_out << fNumber;
+  res_out << fDate;
+  res_out << fComment;
+}
+
+StsSensorBatch::StsSensorBatch(const StsSensorBatch& from){
+  SetCompId(from.GetCompId());
+
+  SetId(from.GetId());
+  SetNumber(from.GetNumber());
+  SetDate(from.GetDate());
+  SetComment(from.GetComment());
+}
+
+StsSensorBatch& StsSensorBatch::operator=(const StsSensorBatch& from){
+  SetCompId(from.GetCompId());
+
+  SetId(from.GetId());
+  SetNumber(from.GetNumber());
+  SetDate(from.GetDate());
+  SetComment(from.GetComment());
+
+  return *this;
+}
+
+TObjArray* StsSensorBatch::GetByNumber(string Number, UInt_t rid)
+{
+  return GetBy(
+    [&Number](StsSensorBatch *inst) -> bool
+      {
+        return Number == inst->GetNumber();
+      });
+}
+
+void StsSensorBatch::FillFromJson(Json::Value& json)
+{
+  SetId(json["Id"].asInt());
+  SetNumber(json["Number"].asString());
+  SetDate(json["Date"].asInt());
+  SetComment(json["Comment"].asString());
+}
+
+void StsSensorBatch::StoreToJson(Json::Value& json)
+{
+  json["Id"] = GetId();
+  json["Number"] = fNumber;
+  json["Date"] = (Int_t) fDate;
+  json["Comment"] = fComment;
+
+  json["Sensors"] = Json::Value(Json::nullValue);
+}
---
+++ dbInterface/FairDbRelationalParSet.h
@@ -0,0 +1,52 @@
+#ifndef FAIRDBRELATIONALPARSET_H
+#define FAIRDBRELATIONALPARSET_H
+
+
+#include "DataType.h"
+#include "FairDbGenericParSet.h"
+#include "TString.h"
+#include "TObjArray.h"
+#include "Rtypes.h"
+#include "json/json.h"
+
+#include <iostream>
+#include <string>
+
+template <typename T>
+class FairDbRelationalParSet : public FairDbGenericParSet<T>
+{
+  public :
+    FairDbRelationalParSet();
+    FairDbRelationalParSet(FairDbDetector::Detector_t detid, DataType::DataType_t dataid, 
+                        const char* name, const char* title, const char* context, Bool_t ownership=kFALSE);
+
+    virtual ~FairDbRelationalParSet(void);
+
+    UInt_t GetIndex(UInt_t /*def*/) const { return fId; }
+    
+    Int_t GetId() const { return fId; }
+    void SetId(Int_t value) { fId = value; this->SetCompId(fId); }
+
+    static T* GetById(Int_t id, UInt_t rid=0);
+    static TObjArray* GetByIds(Int_t* ids, UInt_t count, UInt_t rid=0);
+
+    static T* FromJsonString(string& jsonString);
+    static T* FromJson(Json::Value& json);
+    static TObjArray* FromJsonArray(Json::Value& jsonArray);
+    string ToJsonString();
+    Json::Value ToJson();
+    static Json::Value ToJsonArray(TObjArray* array);
+
+    virtual void FillFromJson(Json::Value& json) { ; }
+    virtual void StoreToJson(Json::Value& json) { ; }
+
+  protected:
+    Int_t fId;
+
+  private:
+    ClassDefT(FairDbRelationalParSet<T>,0); // ROOT Template class definition
+};
+ClassDefT2(FairDbRelationalParSet,T)
+
+#endif /* !FAIRDBRELATIONALPARSET_H */
+
